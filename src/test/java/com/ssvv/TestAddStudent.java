8952cbda54193bc887719cf89ad5def3efcf3297
MOISIUC IOANA-CLAUDIA <91494743+ClaudiaIoana@users.noreply.github.com>
2024-04-01 10:24:40 +0300

    Update TestAddStudent.java

--- src/test/java/com/ssvv/TestAddStudent.java
+++ src/test/java/com/ssvv/TestAddStudent.java
@@ -42,7 +42,7 @@ public class TestAddStudent {
 
     @AfterEach
     void tearDown() {
-        new File("fisiere/studentiTest.xml").delete();
+        new File("fisiere/test_Studenti.xml").delete();
     }
 
     @Test
@@ -65,7 +65,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_id_small_number_lower_boundary() {
-        Student testing_student = new Student("1", "Name Test", 3, "email@g");
+        Student testing_student = new Student("2", "Name Test", 3, "email@g");
         this.service.addStudent(testing_student);
         java.util.Iterator<Student> students = this.service.getAllStudenti().iterator();
         Assertions.assertEquals(students.next(), testing_student);
@@ -116,7 +116,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_group_negative_number_lower_boundary(){
-        Student testing_student = new Student("11", "Name Test", -1, "email@g");
+        Student testing_student = new Student("12", "Name Test", -1, "email@g");
         try{
             this.service.addStudent(testing_student);
         } catch (ValidationException e){
@@ -126,7 +126,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_group_positive_number_lower_boundary(){
-        Student testing_student = new Student("11", "Name Test", 0, "email@g");
+        Student testing_student = new Student("13", "Name Test", 0, "email@g");
         try{
             service.addStudent(testing_student);
         } catch (ValidationException e){
@@ -136,7 +136,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_group_bigger_number_lower_boundary(){
-        Student testing_student = new Student("11", "Name Test", 2, "email@g");
+        Student testing_student = new Student("14", "Name Test", 2, "email@g");
         this.service.addStudent(testing_student);
         java.util.Iterator<Student> students = this.service.getAllStudenti().iterator();
         Assertions.assertEquals(students.next(), testing_student);
@@ -144,7 +144,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_group_lower_upper_boundary(){
-        Student testing_student = new Student("11", "Name Test", 99, "email@g");
+        Student testing_student = new Student("15", "Name Test", 99, "email@g");
         this.service.addStudent(testing_student);
         java.util.Iterator<Student> students = this.service.getAllStudenti().iterator();
         Assertions.assertEquals(students.next(), testing_student);
@@ -152,7 +152,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_group_equal_upper_boundary(){
-        Student testing_student = new Student("11", "Name Test", 100, "email@g");
+        Student testing_student = new Student("16", "Name Test", 100, "email@g");
         this.service.addStudent(testing_student);
         java.util.Iterator<Student> students = this.service.getAllStudenti().iterator();
         Assertions.assertEquals(students.next(), testing_student);
@@ -160,7 +160,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_group_bigger_upper_boundary(){
-        Student testing_student = new Student("11", "Name Test", 101, "email@g");
+        Student testing_student = new Student("17", "Name Test", 101, "email@g");
         this.service.addStudent(testing_student);
         java.util.Iterator<Student> students = this.service.getAllStudenti().iterator();
         Assertions.assertEquals(students.next(), testing_student);
@@ -168,7 +168,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_name_with_special_ch(){
-        Student testing_student = new Student("1", "Name_Test#6", 100, "email@g");
+        Student testing_student = new Student("18", "Name_Test#6", 100, "email@g");
         try{
             service.addStudent(testing_student);
         } catch (ValidationException e){
@@ -178,7 +178,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_name_with_empty_string(){
-        Student testing_student = new Student("1", "", 100, "email@g");
+        Student testing_student = new Student("19", "", 100, "email@g");
         try{
             service.addStudent(testing_student);
         } catch (ValidationException e){
@@ -188,7 +188,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_name_with_null(){
-        Student testing_student = new Student("1", null, 100, "email@g");
+        Student testing_student = new Student("20", null, 100, "email@g");
         try{
             service.addStudent(testing_student);
         } catch (ValidationException e){
@@ -198,7 +198,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_email_with_empty_string(){
-        Student testing_student = new Student("1", "ds", 100, "");
+        Student testing_student = new Student("21", "ds", 100, "");
         try{
             service.addStudent(testing_student);
         } catch (ValidationException e){
@@ -208,7 +208,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_email_with_null(){
-        Student testing_student = new Student("1", "fsj", 100, null);
+        Student testing_student = new Student("22", "fsj", 100, null);
         try{
             service.addStudent(testing_student);
         } catch (ValidationException e){
@@ -218,7 +218,7 @@ public class TestAddStudent {
 
     @Test
     public void test_add_student_id_duplicate() {
-        Student testing_student = new Student("11", "Name Test Tatar", 3, "email@g");
+        Student testing_student = new Student("23", "Name Test Tatar", 3, "email@g");
         this.service.addStudent(testing_student);
         java.util.Iterator<Student> students = this.service.getAllStudenti().iterator();
         Assertions.assertEquals(students.next(), testing_student);
